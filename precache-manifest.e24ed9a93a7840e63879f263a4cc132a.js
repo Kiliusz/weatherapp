27116575ae4bc8e7d98ee9254aeb4c42579ed036
kiliusz <kilonek@gmail.com>
2019-07-30 19:27:03 +0200

    Updates

---
+++ precache-manifest.e24ed9a93a7840e63879f263a4cc132a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa0a718444931b0ba165855bff4acd7",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "da8f4dd15dda7cb98954",
+    "url": "/weatherapp/static/css/main.6d1d52cc.chunk.css"
+  },
+  {
+    "revision": "4e981f48bd6f8ecec239",
+    "url": "/weatherapp/static/js/2.00a643b3.chunk.js"
+  },
+  {
+    "revision": "da8f4dd15dda7cb98954",
+    "url": "/weatherapp/static/js/main.938b05db.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66ed22af7671cceb3c2e92760c6ee281.js"
+  "/weatherapp/precache-manifest.e24ed9a93a7840e63879f263a4cc132a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
