754c7cdd983f3754eaa77d8722fa8074ede66859
kiliusz <kilonek@gmail.com>
2019-07-30 15:45:42 +0200

    Updates

---
+++ precache-manifest.66ed22af7671cceb3c2e92760c6ee281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564da25af6604b6327cd33b764c4765d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a95c7743c616b7cff46",
+    "url": "/static/css/main.6d1d52cc.chunk.css"
+  },
+  {
+    "revision": "4e981f48bd6f8ecec239",
+    "url": "/static/js/2.00a643b3.chunk.js"
+  },
+  {
+    "revision": "3a95c7743c616b7cff46",
+    "url": "/static/js/main.07562160.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
